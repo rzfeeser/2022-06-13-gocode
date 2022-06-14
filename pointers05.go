7b5a219054caa8978fd9ef2dd1dee3134bb7da27
rzfeeser <rzfeeser@users.noreply.github.com>
2022-06-14 16:14:17 +0000

    learning about go maps

--- pointers05.go
+++ pointers05.go
@@ -13,10 +13,10 @@ type User struct {
 }
 
 // a method for type User
-func (p2 *User) newPet() {
-   fmt.Println(*p2, "underlying value of p2 before")
+func (p2 User) newPet() {
+   fmt.Println(p2, "underlying value of p2 before")
    p2.Pet = append(p2.Pet, "Fido")
-   fmt.Println(*p2, "underlying value of p2 after")
+   fmt.Println(p2, "underlying value of p2 after")
 }
 
 func main() {
